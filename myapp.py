4eeafc9a1cf7a4db753b71ba7ad5ca014b9c4429
Rolla-code <52655968+Rolla-code@users.noreply.github.com>
2019-07-18 12:46:52 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -13,4 +13,4 @@ def foo(name):
     return render_template('index.html', to=name)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=port)
